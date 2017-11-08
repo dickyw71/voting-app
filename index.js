f63706624171efb86e27473aca3be259bf388889
dickyw71 <dickyw71@users.noreply.github.com>
2017-11-08 11:07:53 +0000

    Added getAuthorPollData function.

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const data = require('./data.json')
 const app = express()
 
+const getAuthorPollData = (polls, author) => polls.filter(poll => poll.author === author )
+
 app.get('/', (req, res) => res.send(data))
 
+console.log(JSON.stringify(getAuthorPollData(data.polls, "Will White")))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
