4196bba8d34e210c0fc176e5114214a948f3a2a6
dickyw71 <dickyw71@users.noreply.github.com>
2017-11-08 11:31:56 +0000

    Added Closure and function currying for getAuthorPolls.

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const data = require('./data.json')
 const app = express()
 
 const getAuthorPollData = (polls, author) => polls.filter(poll => poll.author === author )
+const getAuthorDataGlobal = (author) => data.polls.filter(poll => poll.author === author )
 
+const getAuthorDataClosure = (polls) => (author) => 
+    polls.filter(poll => poll.author === author )
+
+const getAuthorPolls = getAuthorDataClosure(data.polls)
+    
 app.get('/', (req, res) => res.send(data))
 
-console.log(JSON.stringify(getAuthorPollData(data.polls, "Will White")))
+console.log("2 Params:", JSON.stringify(getAuthorPollData(data.polls, "Will White")))
+console.log("1 Param + Global:", JSON.stringify(getAuthorDataGlobal("Will White")))
+console.log("Closure + 1 Param", JSON.stringify(getAuthorDataClosure(data.polls)("Will White")))
+
+console.log("Fully curried", JSON.stringify(getAuthorPolls("Will White")))
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
