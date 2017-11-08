4bf9a72f5994a69c1a10ab5f6a9634d9da1ed946
dickyw71 <dickyw71@users.noreply.github.com>
2017-11-08 10:40:17 +0000

    Serving data file with Express.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var app = require('express');
+const express = require('express')
+const data = require('./data.json')
+const app = express()
 
+app.get('/', (req, res) => res.send(data))
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
