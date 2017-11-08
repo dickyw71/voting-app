aea86c4b51c2ee12226dfd845e45dbd0d83304b3
dickyw71 <dickyw71@users.noreply.github.com>
2017-11-08 12:03:22 +0000

    Added addPoll function.

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express')
 const data = require('./data.json')
-const app = express()
+const fs = require('fs')
 
-const getAuthorPollData = (polls, author) => polls.filter(poll => poll.author === author )
-const getAuthorDataGlobal = (author) => data.polls.filter(poll => poll.author === author )
+const app = express()
 
 const getAuthorDataClosure = (polls) => (author) => 
     polls.filter(poll => poll.author === author )
 
-const getAuthorPolls = getAuthorDataClosure(data.polls)
-    
+// Example of Function Currying    
+// const getAuthorPolls = getAuthorDataClosure(data.polls)
+ 
+const addPoll = (data) => (newPoll) => {
+    const newPolls = data.polls.concat(newPoll);
+    console.log(Object.assign(data, { polls: newPolls }))
+    // fs.writeFileSync("./data.json", Object.assign(data, { polls: newPolls }))
+}
+
 app.get('/', (req, res) => res.send(data))
 
-console.log("2 Params:", JSON.stringify(getAuthorPollData(data.polls, "Will White")))
-console.log("1 Param + Global:", JSON.stringify(getAuthorDataGlobal("Will White")))
-console.log("Closure + 1 Param", JSON.stringify(getAuthorDataClosure(data.polls)("Will White")))
-
-console.log("Fully curried", JSON.stringify(getAuthorPolls("Will White")))
-
+addPoll(data) ({ 
+    "title": "ML frameworks",
+    "author": "Elon Musk",
+    "options": [
+        {
+            "option": "Python",
+            "votes": 3
+        },
+        {
+            "option": "R",
+            "votes": 1
+        }
+    ]
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
