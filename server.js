01cf3d17eced470efd5e1c8fa6b61535b1870baa
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-11-15 15:35:40 +0000

    Fixed bugs with poll id and moved initial data.json file to new test folder.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const data = require("./data.json");
+const data = require("./test/data.json");
 const fs = require("fs");
 
 const app = express();
@@ -9,36 +9,41 @@ app.use(express.static("public"));
 
 // Get the polls for a specific author
 const getAuthorData = polls => author =>
-  polls.filter(poll => poll.author === author);
+    polls.filter(poll => poll.author === author);
 const getAuthorPolls = getAuthorData(data.polls);
 
 // Create poll data
 const addPoll = data => newPoll => {
-  const newPolls = data.polls.concat(newPoll);
-  fs.writeFileSync(
-    "./data.json",
-    JSON.stringify(
-      Object.assign(data, { polls: newPolls, id: data.id + 1 }),
-      null,
-      2
-    ),
-    "utf-8"
-  );
+    newPoll.id = data.nextPollId;
+    data.nextPollId++;
+    const newPolls = data.polls.concat(newPoll);
+    fs.writeFileSync(
+        "./data.json",
+        JSON.stringify(
+            Object.assign(data, { 
+                polls: newPolls, 
+                nextPollId: data.nextPollId 
+            }),
+            null,
+            2
+        ),
+        "utf-8"
+    );
 };
 addPoll(data)({
-  id: 1, // FIXME
-  title: "ML frameworks",
-  author: "Elon Musk",
-  options: [
-    {
-      option: "Python",
-      votes: 3
-    },
-    {
-      option: "R",
-      votes: 1
-    }
-  ]
+    id: undefined,  // id will be set when poll is added
+    title: "ML frameworks",
+    author: "Elon Musk",
+    options: [
+        {
+            option: "Python",
+            votes: 3
+        },
+        {
+            option: "R",
+            votes: 1
+        }
+    ]
 });
 
 // Read poll data
@@ -46,40 +51,40 @@ app.get("/polldata", (req, res) => res.send(data));
 
 // Update poll data - add option
 const updatePollOptions = data => pollData => {
-  const newPolls = data.polls.map(poll => {
-    if (poll.id === pollData.id) {
-      // make sure option doesn't already exist
-      if (
-        poll.options.filter(option => option.option === pollData.newOption)
-          .length === 0
-      ) {
-        poll.options = poll.options.concat({
-          option: pollData.newOption,
-          votes: 0
-        });
-      }
-    }
-    return poll;
-  });
-  fs.writeFileSync(
-    "./data.json",
-    JSON.stringify(Object.assign(data, { polls: newPolls }), null, 2),
-    "utf-8"
-  );
+    const newPolls = data.polls.map(poll => {
+        if (poll.id === pollData.id) {
+            // make sure option doesn't already exist
+            if (
+                poll.options.filter(option => option.option === pollData.newOption)
+                    .length === 0
+            ) {
+                poll.options = poll.options.concat({
+                    option: pollData.newOption,
+                    votes: 0
+                });
+            }
+        }
+        return poll;
+    });
+    fs.writeFileSync(
+        "./data.json",
+        JSON.stringify(Object.assign(data, { polls: newPolls }), null, 2),
+        "utf-8"
+    );
 };
 const updatePollAddOption = updatePollOptions(data);
-updatePollAddOption({ id: 0, newOption: "Scala" });
+updatePollAddOption({ id: 1, newOption: "Scala" });
 
 // Delete poll data
 const deletePollData = data => pollId => {
-  const newPolls = data.polls.filter(poll => poll.id !== pollId);
-  fs.writeFileSync(
-    "./data.json",
-    JSON.stringify(Object.assign(data, { polls: newPolls }), null, 2),
-    "utf-8"
-  );
+    const newPolls = data.polls.filter(poll => poll.id !== pollId);
+    fs.writeFileSync(
+        "./data.json",
+        JSON.stringify(Object.assign(data, { polls: newPolls }), null, 2),
+        "utf-8"
+    );
 };
 const deletePoll = deletePollData(data);
-deletePoll(2);
+deletePoll(3);
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
